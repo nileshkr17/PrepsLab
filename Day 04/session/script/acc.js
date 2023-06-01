687fca23d0fed8144390afa689f2063ae5a87826
Nilesh <ece.20becd76@silicon.ac.in>
2023-06-01 14:38:12 +0530

    [POST-method-Working-Fine]

--- Day 04/session/script/acc.js
+++ Day 04/session/script/acc.js
@@ -1,16 +1,32 @@
 const button = document.getElementById("mybutton");
+const error = document.getElementById("error");
+const sucess = document.getElementById("sucess");
 button.addEventListener("click", () => {
   const emailinput = document.getElementById("email").value;
   const pass = document.getElementById("password").value;
-  fetch("http://localhost:3004/employees", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: emailinput,
-      password: pass,
+  if (emailinput != "" || pass != "") {
+    fetch("http://localhost:3004/employees", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: Date.now(),
+        email: emailinput,
+        password: pass,
+      }),
     })
-  });
-  console.log(emailinput, pass); 
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        alert("Account created sucessfully");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  } else {
+    error.innerHTML = "Please fill all the fields";
+  }
+  console.log(emailinput, pass);
 });
+
