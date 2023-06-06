514a7629c4a5b0154fcf19c87366c7670bc25c3e
Nilesh <ece.20becd76@silicon.ac.in>
2023-06-06 19:44:34 +0530

    [added-toggle]

--- Day 06/new/src/components/Toggle.jsx
+++ Day 06/new/src/components/Toggle.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './toggle.css'
 const Toggle = () => {
+   
   return (
     // toggle button
     <>
         <div className="toggle">
             <input type="checkbox" id="toggle" />
-            <label htmlFor="toggle"></label>
+            <label htmlFor="toggle" id="input-toggle"></label>
         </div>
     </>
   )
+
 }
 
 export default Toggle
