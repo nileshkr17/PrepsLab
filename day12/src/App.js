b4fbe94ece4c7d8caa9666960f9bbc4349375128
Nilesh <ece.20becd76@silicon.ac.in>
2023-06-13 11:01:44 +0530

    []

--- day12/src/App.js
+++ day12/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Allroutes from './Allroutes';
+import Nav from './pages/Nav/Nav';
 
 
 function App() {
   return (
+    <>
+     <Nav/>
     <Allroutes/>
+    </>
+   
   );
 }
 
--- day12/src/pages/Nav/Nav.jsx
+++ day12/src/pages/Nav/Nav.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Nav = () => {
+    const navigate = useNavigate();
+    const {search,updateSearch} =  useState();
+
+
   return (
+
+    <>
     <div className='navbar'>
-        
+        <div className='left'>
+            <h2>Logo</h2>
+        </div>
+        <div className='right'>
+            <ul>
+                <button></button>
+            </ul>
+        </div>
     </div>
+    </>
   )
 }
 
