3a75ee99c5b16a103e777b70d52b90c83fe66ff6
Nilesh <ece.20becd76@silicon.ac.in>
2023-06-13 09:55:05 +0530

    []

--- Assignment(after 9)/day09-movieSearch/src/App.js
+++ Assignment(after 9)/day09-movieSearch/src/App.js
@@ -21,7 +21,7 @@ function App() {
   //   setMovies(data.Search);
   // };
 
-  const fetcgMovies = async (searchValue) => {
+  const fetchMovies = async (searchValue) => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users",{
     method: "GET",
     headers: {
