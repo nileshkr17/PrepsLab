aa12d245bfa2855c0f9b8ccfe1d4780062a1096d
Nilesh <ece.20becd76@silicon.ac.in>
2023-06-20 12:12:09 +0530

    [added-mongo]

--- NodeDay1/routes/UserRoute.js
+++ NodeDay1/routes/UserRoute.js
@@ -1,3 +1,12 @@
 const express = require('express');
+const userRoute = require('../Routes/UserRoute');
 const {UserModel} = require('../models/UserModel');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const userRouter = express.Router();
+
+userRouter.get('/', async (req, res) => {
+
+});
 
--- NodeDay1/server.js
+++ NodeDay1/server.js
@@ -1,25 +1,29 @@
-// import port from .env
-require('dotenv').config();
+const express = require('express');
+const {dbcnt} = require('./main');
+// const {UserModel} = require('./models/UserModel');
+// const {userRoute} = require('./Routes/UserRoute');
+const cors = require('cors');
+const app = express();
 
-const http = require('http');
+app.use(express.json());
+app.use(cors());
 
-const server = http.createServer((req,res)=>{
-    res.end('Hello World');
+// app.get("/users",userRoute);
+
+// app.use("/user",userRoute);
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+}
+);
+
+app.listen(process.env.PORT ,async () => {
+  try{
+    await dbcnt();
+    console.log(`DB Connected `);
+    console.log(`Server Started ${process.env.PORT}`);
+
+  }catch(error){
+    console.log("DB Connection Failed");
+  }
 });
-
-server.listen(dot,()=>{
-    console.log('Server is running on port 7888');
-});
-
-// fs
-
-// path
-// os
-// http
-// crypto
-// events
-// show each example
-
-
-
-
