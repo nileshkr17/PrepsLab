b73ad2663896fe87e4181ac0f32461f9de81c0dc
Nilesh <ece.20becd76@silicon.ac.in>
2023-06-21 10:19:49 +0530

    [added-usermodel]

---
+++ NodeDay1/models/UserModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  gender: String,
+  pass: String,
+  mobile: Number,
+});
+const UserModel = mongoose.model("user", UserSchema);
+module.exports = { UserModel };
+
+
+// const UserModel: This creates a constant variable named UserModel to store the reference to the model object.
+// mongoose.model: This is a method provided by Mongoose to create a model based on a defined schema. 
+// It takes two arguments: the name of the model (in this case, "User") and the schema definition (in this case, UserSchema).
+// "User": This is the name given to the model. It will be used to identify the collection in the MongoDB database.
+// UserSchema: This refers to a previously defined schema for the user object. 
+//The schema defines the structure, fields, and validation rules for the user documents in the "User" collection.
